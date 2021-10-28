90187a2b2714a103177d818d30b8ab368d1befaf
LebronAl <TXYPotato@gmail.com>
2021-10-29 00:14:25 +0800

    fix

--- driver.c
+++ driver.c
@@ -252,7 +252,7 @@ int main(int argc, char *argv[]) {
 
   winograd_init(layer_num, Batch_arr, C_arr, H_arr, W_arr, K_arr);
 
-  printf(1)
+  printf(1);
 
   for (int l = 0; l < layer_num; l++) {
     winograd_conv(l, validation_mode, H_arr[l], W_arr[l], C_arr[l], K_arr[l],
